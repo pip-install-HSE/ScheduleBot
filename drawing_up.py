09eb6f3371e42f0d43d1599dc5657e728d32c597
6a16ec <semaev2014@yandex.ru>
2021-12-12 16:44:09 +0300

    Added importance to the model and refactored the code

--- drawing_up.py
+++ drawing_up.py
@@ -2,9 +2,10 @@ from openpyxl import Workbook
 from slots_sort import sort_slots
 import re
 
+
 def alg1(stafftmp, staffdatatmp, slotstmp, sh1tmp):
     smeni = []
-    for i in range(0,staff):
+    for i in range(0, staff):
         smeni.append(0)
     for i in range(0, slotstmp):
         smeni1 = []
@@ -16,26 +17,26 @@ def alg1(stafftmp, staffdatatmp, slotstmp, sh1tmp):
             if smeni1[j] == 0:
                 smeni1[j] = 999
 
-        smeni1_num = list (enumerate(smeni1, 0))
+        smeni1_num = list(enumerate(smeni1, 0))
 
         r = min(smeni1_num, key=lambda g: g[1])
 
-        del(smeni1_num[r[0]])
-        if r[1]!=999:
-
-            smeni[r[0]]+=1
+        del (smeni1_num[r[0]])
+        if r[1] != 999:
+            smeni[r[0]] += 1
         r2 = min(smeni1_num, key=lambda g: g[1])
-        if r2[1] != 999 and r2[0]!=r[0]:
+        if r2[1] != 999 and r2[0] != r[0]:
             smeni[r2[0]] += 1
-            l = [i, r[0],r2[0]]
+            l = [i, r[0], r2[0]]
 
         else:
-            l = [i,r[0],'-']
-        if r[1]==999:
-            l = [i,'-','-']
+            l = [i, r[0], '-']
+        if r[1] == 999:
+            l = [i, '-', '-']
         sh1tmp.append(l)
 
-    #sh1tmp.append(smeni)
+    # sh1tmp.append(smeni)
+
 
 def alg2(stafftmp, staffdatatmp, slotstmp, sh1tmp):
     smeni = []
@@ -51,23 +52,24 @@ def alg2(stafftmp, staffdatatmp, slotstmp, sh1tmp):
                 smeni1[j] += 1 + smeni[j]
             if smeni1[j] == 0:
                 smeni1[j] = 999
-        smeni1_num = list (enumerate(smeni1, 0))
+        smeni1_num = list(enumerate(smeni1, 0))
 
         r = min(smeni1_num, key=lambda g: g[1])
-        del(smeni1_num[r[0]])
-        if r[1]!=999:
-            smeni[r[0]]+=1
+        del (smeni1_num[r[0]])
+        if r[1] != 999:
+            smeni[r[0]] += 1
         r2 = min(smeni1_num, key=lambda g: g[1])
-        if r2[1] != 999 and r2[0]!=r[0]:
+        if r2[1] != 999 and r2[0] != r[0]:
             smeni[r2[0]] += 1
-            l = [i, r[0],r2[0]]
+            l = [i, r[0], r2[0]]
         else:
-            l = [i, r[0],'-']
+            l = [i, r[0], '-']
         if r[1] == 999:
             l = [i, '-', '-']
         sh1tmp.append(l)
 
-    #sh1tmp.append(smeni)
+    # sh1tmp.append(smeni)
+
 
 def func(thread_name, staff_priority, staff_flows):
     result = 0
@@ -76,11 +78,12 @@ def func(thread_name, staff_priority, staff_flows):
             result += 1
     return result
 
+
 if __name__ == '__main__':
     staff_schedule, staff_load, staff_threads, thread_names, staff_flows = [], [], [], [], []
     staff_number, threads_number = map(int, input().split())
-    thread_names = re.sub(r'\s*','',input()).split(',')
-    input()
+    thread_names = re.sub(r'\s+', '', input()).split(',')
+    thread_importance = list(map(int, re.sub(r'\s+', '', input()).split(',')))
     staff_names = []
     print(thread_names)
     for num in range(1, staff_number + 1):
@@ -103,15 +106,16 @@ if __name__ == '__main__':
     worksheet = workbook.active
 
     for i in range(28):
-        worksheet.cell(row=i+2, column=1).value = f"Смена {i+1}"
+        worksheet.cell(row=i + 2, column=1).value = f"Смена {i + 1}"
     for i in range(threads_number):
-        worksheet.cell(row=1, column=i+2).value = thread_names[i]
+        worksheet.cell(row=1, column=i + 2).value = thread_names[i]
 
     list_slots = sort_slots(staff_schedule)
     staff_sum = [0 for _ in range(staff_number)]
     staff_slots = [[] for _ in range(staff_number)]
     threads_sum = [0 for _ in range(threads_number)]
     for slot in list_slots:
+        print("slot: ", slot)
         staff_1 = []
         staff_2 = []
         for staff in range(staff_number):
@@ -128,36 +132,57 @@ if __name__ == '__main__':
             for _ in range(staff_threads[staff] - 1):
                 staff_2.append(staff)
         staff_priority = staff_2 + staff_1
-        staff_priority = list(filter(lambda staff: staff_sum[staff] < staff_load[staff], staff_priority))
-        #print(staff_priority)
         staff_priority = list(
-            filter(lambda staff: not ((slot - 1) in staff_slots[staff] and (slot - 2) in staff_slots[staff]),
-                staff_priority))
+            filter(lambda staff: staff_sum[staff] < staff_load[staff],
+                   staff_priority))
+        # print(staff_priority)
         staff_priority = list(
-            filter(lambda staff: not ((slot + 1) in staff_slots[staff] and (slot + 2) in staff_slots[staff]),
-                 staff_priority))
+            filter(lambda staff: not (
+                        (slot - 1) in staff_slots[staff] and (slot - 2) in
+                        staff_slots[staff]),
+                   staff_priority))
         staff_priority = list(
-            filter(lambda staff: not ((slot - 1) in staff_slots[staff] and (slot + 1) in staff_slots[staff]),
-                staff_priority))
+            filter(lambda staff: not (
+                        (slot + 1) in staff_slots[staff] and (slot + 2) in
+                        staff_slots[staff]),
+                   staff_priority))
+        staff_priority = list(
+            filter(lambda staff: not (
+                        (slot - 1) in staff_slots[staff] and (slot + 1) in
+                        staff_slots[staff]),
+                   staff_priority))
         if slot % 4 == 0:
             staff_priority = list(
                 filter(lambda staff: not ((slot + 1) in staff_slots[staff]),
-                        staff_priority))
+                       staff_priority))
         if slot % 4 == 1:
             staff_priority = list(
                 filter(lambda staff: not ((slot - 1) in staff_slots[staff]),
                        staff_priority))
-        #print(staff_priority)
+        # print(staff_priority)
         threads_priority = list(range(threads_number))
-        threads_priority = sorted(threads_priority, key=lambda thread: threads_sum[thread])
-        threads_priority = sorted(threads_priority, key=lambda thread: func(thread_names[thread], set(staff_priority), staff_flows))
+        threads_priority = sorted(threads_priority,
+                                  key=lambda thread: threads_sum[thread])
+        threads_priority = sorted(threads_priority,
+                                  key=lambda thread: func(thread_names[thread],
+                                                          set(staff_priority),
+                                                          staff_flows))
         for thread in threads_priority:
+            print("thread: ", thread)
+            print(thread, staff_priority)
+            maybe_twice = []
             staff = None
             for i, staff_x in enumerate(staff_priority):
                 if thread_names[thread] in staff_flows[staff_x]:
                     staff = staff_x
                     staff_priority.pop(i)
+                    if thread_importance[thread] == 2:
+                        if staff in staff_priority:
+                            staff_priority.remove(staff)
+                    else:
+                        maybe_twice.append(staff)
                     break
+            print("staff:", staff, staff_priority)
             if staff is not None:
                 # worksheet.cell(row=slot+2, column=thread+2).value = f" Сотрудник {staff+1}"
                 worksheet.cell(row=slot + 2,
@@ -168,7 +193,6 @@ if __name__ == '__main__':
         print(staff_sum)
 
     workbook.save("result.xlsx")
-
 
 # wb = Workbook()
 #
